41986c930a13d9d5b2b6cc6c42685ccffdb69893
tunckasik <tunckasikali@gmail.com>
2023-01-27 12:04:26 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "2.36.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+data "template_file" "userdata" {
+  template = file("${abspath(path.module)}/userdata.sh")
+}
+
+ resource "aws_instance" "vm1" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.HTTPandSSH.id]
+  user_data = data.template_file.userdata.rendered
+  key_name = "dockerkey"
+ }
+
+ resource "aws_security_group" "HTTPandSSH" {
+  name        = "HTTPandSSH"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  ingress {
+    description      = "allow HTTP"
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description      = "allow SSH"
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "allow Jenkins"
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "allow 50000"
+    to_port          = 50000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
---
+++ userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo yum update -y
+# install docker
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# install git
+sudo yum install git -y
+# install jenkins
+sudo git clone https://github.com/tunckasik/terraform-modules-jenkins.git /home/ec2-user/jenkins_deployment_terraform
+docker build -t jenkins /home/ec2-user/jenkins/dockerfile
+docker run -p 8080:8080 -p 50000:50000 -d --restart=on-failure -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/datadir:/var/jenkins_home --name jenkins-nodejs-dind jenkins
